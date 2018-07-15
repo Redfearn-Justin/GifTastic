17f04013b8e625f8ced8d0bc49736490c33b81fd
Napoleon <wanderofrazgriz@gmail.com>
2018-07-14 21:25:43 -0700

    Updated JS/HTML

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,6 @@ $(document).ready(function() {
 
     var topics = ["Captain America", "Thor", "The Hulk", "Ironman"];
 
-    //take indexes in "topics" array and create buttons
-       ///you should use a for loop (each jQ method) for this
-
-
     //functions
 
     function constructButtons() {
@@ -22,7 +18,7 @@ $(document).ready(function() {
 
             // Altering attributes of generateButton
 
-            generateButton.addClass("btn btn-outline-danger");
+            generateButton.addClass("btn btn-outline-danger charButtons");
 
             generateButton.attr("data-name", topics[j]);
 
@@ -37,7 +33,7 @@ $(document).ready(function() {
 
     $("#clickToAdd").on("click", function() {
 
-        //event.preventDefault();
+        event.preventDefault();
 
         console.log("button pressed");
 
@@ -60,39 +56,64 @@ $(document).ready(function() {
         constructButtons();
     });
 
+    $("#buttonsGoHere").on("click", "button", function() {
+
+        console.log("Character Button Pressed");
+
+        var charName = $(this).attr("data-name");
+        console.log(this);
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+          charName + "&api_key=pgX0i1wv9Kowb6SQamm5VYObKPhQYP6F&limit=10";
+  
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+          .then(function(json) {
+
+            var results = json.data;
+
+            console.log(json);
+  
+            for (var j = 0; j < results.length; j++) {
+              
+              var gifDiv = $("<div>");
+  
+              var rating = results[j].rating;
+  
+              var ratingContainer = $("<p>").text("Rating: " + rating);
+  
+              var charGif = $("<img>");
+
+              charGif.attr("src", results[j].images.fixed_height.url);
+  
+              gifDiv.prepend(charGif).append(ratingContainer);
+  
+              $("#gifsGoHere").prepend(gifDiv);
+            }
+          });
+
+
+    });
+
 
     //function calls
 
+    //to initialize page with buttons
     constructButtons();
-
-
-
-
-    //on click function when user clicks button: summons 10 gifs of said hero/villian
 
     
     //on click function so when user clicks on a gif, it animates
         //!!!! gifs should not automatically load upon load!!
         //if clicked again, should stop gif
 
-    //manifest function that will:
-        //A) "push" the new entry into the array
-        //B) a for loop (each jQ method) that will cycle through the array and create new buttons (should only be the newest addition to pop up)
-
-    //under every gif, display its rating 
-        //this information is supplied by Giphy
-
-
-
-
 
 }); //document ready function
 
 
 //other notes
-// 1) make sure to use the following Giphy parameters: q, limit (for how many gifs to appear), and rating (to display the rating)
+
 // 2) Update README
 // 3) attach to Portfolio
-//  use .val().trim() when getting input from "add here" field
-//  remember to use "this"!!!
+
 // IF FINISH EARLY: EDIT PAGE WITH CSS!! ADD SOUNDS? REFER TO OTHER BONUS OBJECTIVES

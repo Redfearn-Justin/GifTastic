973025d8452b5e04926f76f10fc6a42a8fed4835
Napoleon <wanderofrazgriz@gmail.com>
2018-07-14 17:28:41 -0700

    pseudo-coded JS

---
+++ assets/javascript/app.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+
+    //variables 
+
+    var topics = ["Captain America", "Thor", "The Hulk", "Ironman"];
+
+    //take indexes in "topics" array and create buttons
+       ///you should use a for loop (each jQ method) for this
+
+    //on click function when user clicks button: summons 10 gifs of said hero/villian
+
+    
+    //on click function so when user clicks on a gif, it animates
+        //!!!! gifs should not automatically load upon load!!
+        //if clicked again, should stop gif
+
+    //manifest function that will:
+        //A) "push" the new entry into the array
+        //B) a for loop (each jQ method) that will cycle through the array and create new buttons (should only be the newest addition to pop up)
+
+    //under every gif, display its rating 
+        //this information is supplied by Giphy
+
+
+
+
+
+}); //document ready function
+
+
+//other notes
+// 1) make sure to use the following Giphy parameters: q, limit (for how many gifs to appear), and rating (to display the rating)
+// 2) Update README
+// 3) attach to Portfolio
+//  use .val().trim() when getting input from "add here" field
+//  remember to use "this"!!!
